1c82c91f5a6659684f8f4ca88bb08b5e76a0383c
gustavofmacia <gustavofmacia@gmail.com>
2023-08-14 22:34:49 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./Components/Card";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
