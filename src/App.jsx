8fe556c914d559888af1a3a2ecc8811c495f1eea
gustavofmacia <gustavofmacia@gmail.com>
2023-08-16 22:25:54 -0300

    Form funcionando con sus respectivos input. Card que muestra los datos cuando se ingresan correctamente los datos en elos inputs. Mensaje de error en caso de que se ingresen incorrectamente los datos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
+
 import "./App.css";
 import Card from "./Components/Card";
 
 export default function App() {
+  const [heroe, setHeroe] = useState({
+    nombre: "",
+    direccion: "",
+  });
+
+  const [showCard, setShowCard] = useState(false);
+  const [error, setError] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (heroe.nombre.length >= 3 && heroe.direccion.length >= 6) {
+      console.log("first");
+      setShowCard(true);
+      setError(false);
+    } else {
+      setShowCard(false);
+      setError(true);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Carga los datos de tu héroe favorito</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingresa el nombre"
+          onChange={(e) => setHeroe({ ...heroe, nombre: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Ingresa su dirección"
+          onChange={(e) => setHeroe({ ...heroe, direccion: e.target.value })}
+        />
+        <button>Guardar datos</button>
+      </form>
+      {showCard && <Card datos={heroe} />}
+      {error && (
+        <p style={{ color: "red", fontWeight: "500" }}>
+          Por favor chequea que la información ingresada sea correcta.
+        </p>
+      )}
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,3 +1,16 @@
-export default function Card() {
-  return <div>Hola, soy una tarjeta</div>;
+import PropTypes from "prop-types";
+
+Card.propTypes = {
+  datos: PropTypes.object,
+};
+
+export default function Card(props) {
+  return (
+    <div style={{ padding: 20, border: "1px solid black", borderRadius: 10 }}>
+      <p style={{ color: "black", fontSize: 20 }}>
+        Tu heroe favorito se llama {props.datos.nombre} y vive en{" "}
+        {props.datos.direccion}
+      </p>
+    </div>
+  );
 }
